4980801bc0abdc02fa0b8334f69b78e9b9e60f8d
dudustri <eduardostrindade@gmail.com>
2024-02-02 14:08:01 +0100

    fix: binary assertion in the data response test

--- itu-minitwit/minitwit.py
+++ itu-minitwit/minitwit.py
@@ -17,7 +17,7 @@ from datetime import datetime
 from contextlib import closing
 from flask import Flask, request, session, url_for, redirect, \
      render_template, abort, g, flash
-from werkzeug import check_password_hash, generate_password_hash
+from werkzeug.security import check_password_hash, generate_password_hash
 
 
 # configuration
@@ -38,7 +38,7 @@ def connect_db():
 def init_db():
     """Creates the database tables."""
     with closing(connect_db()) as db:
-        with app.open_resource('schema.sql') as f:
+        with app.open_resource('schema.sql', mode='r') as f:
             db.cursor().executescript(f.read())
         db.commit()
 
