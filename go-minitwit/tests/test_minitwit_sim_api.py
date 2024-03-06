e53baae691a05fde6186e1d511f3c69936de77bc
Simon Harwick <harwick@hdm.edu>
2024-03-06 22:37:25 +0100

    feat: added minitwit api test to test package. Changed path. Attempt 3

--- go-minitwit/tests/test_minitwit_sim_api.py
+++ go-minitwit/tests/test_minitwit_sim_api.py
@@ -11,8 +11,10 @@ from contextlib import closing
 BASE_URL = "http://localhost:8081/api"
 if os.getenv('EXECUTION_ENVIRONMENT') == 'CI':
     DATABASE = "../tmp/minitwit_empty.db"
+    PATH_SCHEMA = "../schema.sql"
 else:
-    DATABASE = "../tmp/minitwit_empty.db"
+    PATH_SCHEMA = "./schema.sql"
+    DATABASE = "./tmp/minitwit_empty.db"
 USERNAME = 'simulator'
 PWD = 'super_safe!'
 CREDENTIALS = ':'.join([USERNAME, PWD]).encode('ascii')
@@ -22,10 +24,11 @@ HEADERS = {'Connection': 'close',
            f'Authorization': f'Basic {ENCODED_CREDENTIALS}'}
 
 
+
 def init_db():
     """Creates the database tables."""
     with closing(sqlite3.connect(DATABASE)) as db:
-        with open("go-minitwit/schema.sql") as fp:
+        with open(PATH_SCHEMA) as fp:
             db.cursor().executescript(fp.read())
         db.commit()
 
