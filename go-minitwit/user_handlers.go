631bd5dd1f1197eafd4f049dcd0993f147ff01db
Ghmeding <meding97@gmail.com>
2024-03-15 20:13:21 +0100

    fix: proper error-handling an empty message

--- go-minitwit/user_handlers.go
+++ go-minitwit/user_handlers.go
@@ -200,13 +200,14 @@ func addMessageHandler(c *gin.Context) {
 		text := c.Request.FormValue("text")
 
 		if text == "" {
-			errorData = "You have to enter a value"
-			c.Redirect(http.StatusBadRequest, "/?error="+errorData)
+			c.Redirect(http.StatusSeeOther, "/")
+			session.AddFlash("You have to enter a value")
+			session.Save()
 			return
 		} else {
 			err := addMessage(text, userIDString)
 			if err != nil {
-				errorData = "Failed to register user"
+				errorData = "Failed to add message"
 				c.Redirect(http.StatusInternalServerError, "/?error="+errorData)
 				return
 			}
