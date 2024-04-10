bd69e2face3b71749b47c6852699507b22b8018c
Tomas Babkine-Di Caprio <tbab@itu.dk>
2024-04-10 18:53:15 +0200

    fix: refs

--- remote_files/deploy.sh
+++ remote_files/deploy.sh
@@ -3,12 +3,10 @@
 cd /minitwit || exit
 
 touch .env
-echo "DBUSER=$1">.env
+echo "DBUSER=$1">.env #only one '>' = overwrite .env if existing
 echo "DBPASS=$2">>.env
 
-docker compose -f docker-compose.yml down
-sleep 10s
-echo "pulling minitwit image"
-docker pull mihr/minitwitimage
-echo "starting the docker compose"
+docker image rm mihr/minitwitimage
+docker compose -f docker-compose.yml pull
 docker compose -f docker-compose.yml up -d --remove-orphans
+#docker pull $DOCKER_USERNAME/flagtoolimage:latest
