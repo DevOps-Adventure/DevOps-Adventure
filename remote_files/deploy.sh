5eb264031efd33e46913018fa32f542d715b3552
dudustri <eduardostrindade@gmail.com>
2024-04-10 17:57:29 +0200

    fix: add docker compose down and sleep to deployment bash script

--- remote_files/deploy.sh
+++ remote_files/deploy.sh
@@ -3,10 +3,10 @@
 cd /minitwit || exit
 
 touch .env
-echo "DBUSER=$1">.env #only one '>' = overwrite .env if existing
+echo "DBUSER=$1">.env
 echo "DBPASS=$2">>.env
 
-
+docker compose -f docker-compose.yml down
+sleep 10s
 docker compose -f docker-compose.yml pull
 docker compose -f docker-compose.yml up -d --remove-orphans
-#docker pull $DOCKER_USERNAME/flagtoolimage:latest
