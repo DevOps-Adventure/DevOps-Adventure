6f3f046a8b4e040eeef5358007aa0339b3edd8b4
dudustri <eduardostrindade@gmail.com>
2024-06-09 15:13:54 +0200

    fix: CD 2 bash script

--- swarm_files/stack.sh
+++ swarm_files/stack.sh
@@ -5,10 +5,17 @@ echo "DBUSER=$1">.env
 echo "DBPASS=$2">>.env
 
 #setting the env
-set -a; . ./.env; set +a
+if [ -f ./.env ]; then
+    set -a
+    source ./.env
+    set +a
+else
+    echo ".env file not found!"
+    exit 1
+fi
 
 # stacking the compose in the swarm
-docker stack deploy -c docker-compose.yml stack_name
+docker stack deploy -c docker-compose.yml llama_service
 
 #rolling update image of minitwit
-docker service update --image mihr/minitwitimage:latest stack_name_minitwit 
+docker service update --image mihr/minitwitimage:latest llama_service_minitwit 
